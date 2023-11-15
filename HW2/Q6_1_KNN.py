fbcb993a0846f4db2f1319173174fe256891a83c
alifthi <alifathi8008@gmail.com>
2023-11-14 21:37:24 -0600

    machinelearning_homeworks: transform method developed

--- HW2/Q6_1_KNN.py
+++ HW2/Q6_1_KNN.py
@@ -6,12 +6,23 @@ class KNN:
     def fit(self,data,targets):
         self.data=data
         self.targets=targets
-    def transform(self,data):
-        
-        pass
-    
+        self.classes=list(set(self.targets))
+    def transform(self,inp_data): 
+        dists=np.abs(self.data@inp_data.T)
+        predictions=[]
+        for i in range(dists.shape[1]):
+            arg_mins=np.argpartition(dists[:,i],self.K)
+            candidates=self.targets[arg_mins[:self.K]]
+            num_of_nearest_classes={}
+            for c in self.classes:
+                num_of_nearest_classes.update({c:np.sum(candidates==c)})
+            predictions.append(max(num_of_nearest_classes,key=num_of_nearest_classes.get))
+        return predictions
 if __name__=='__main__':
     model=KNN(K=5)
     data=np.random.normal(0,1,[10,10])
     targets=np.array([1,1,1,1,1,0,0,0,0,0])
-    
+    a=np.random.normal(0,1,[2,10])
+    model.fit(data,targets)
+    prediction=model.transform(a)
+    print(prediction)
