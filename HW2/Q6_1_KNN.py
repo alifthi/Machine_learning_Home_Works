c16b48771f6ef8d611b8b7e1b100bfac8f026039
alifthi <alifathi8008@gmail.com>
2023-11-14 23:16:40 -0600

    machinelearning_homeworks:

--- HW2/Q6_1_KNN.py
+++ HW2/Q6_1_KNN.py
@@ -8,21 +8,35 @@ class KNN:
         self.targets=targets
         self.classes=list(set(self.targets))
     def transform(self,inp_data): 
-        dists=np.abs(self.data@inp_data.T)
         predictions=[]
-        for i in range(dists.shape[1]):
-            arg_mins=np.argpartition(dists[:,i],self.K)
+        for i in range(inp_data.shape[0]):
+            distances=[]
+            for j in range(self.data.shape[0]):
+                distances.append(np.linalg.norm(inp_data[i]-self.data[j]))
+                
+            # arg_mins=np.argpartition(dists[i,:],100)
+            arg_mins=sorted(range(len(distances)), key=lambda sub: distances[sub])[:self.K]
             candidates=self.targets[arg_mins[:self.K]]
             num_of_nearest_classes={}
             for c in self.classes:
                 num_of_nearest_classes.update({c:np.sum(candidates==c)})
             predictions.append(max(num_of_nearest_classes,key=num_of_nearest_classes.get))
         return predictions
-if __name__=='__main__':
-    model=KNN(K=5)
-    data=np.random.normal(0,1,[10,10])
-    targets=np.array([1,1,1,1,1,0,0,0,0,0])
-    a=np.random.normal(0,1,[2,10])
-    model.fit(data,targets)
-    prediction=model.transform(a)
-    print(prediction)
+# if __name__=='__main__':
+import numpy as np
+from sklearn.datasets import load_iris
+from matplotlib import pyplot as plt
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+iris=load_iris()
+data=iris['data']
+targets=iris['target']
+
+train_data,test_data,train_targets,test_targets=train_test_split(data,targets,test_size=0.2)
+model=KNN(K=5)
+model.fit(train_data,train_targets)
+prediction=model.transform(test_data)
+print(prediction)
+
+
